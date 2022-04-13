ed32d1a8f03670ad57c3d960ddc2969e022f6262
NMZ-TripleS <nayminzawoffical@gmail.com>
2022-04-13 22:18:08 +0630

    Latest

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -10,21 +10,22 @@ export default class HomeScreen extends React.Component{
     this.state={
       todoList : [
         {title:"Sample Title",descripiton:"sample Description"},
-        {title:"Sample Title1",descripiton:"sample Description"}],
+        {title:"Sample Title1",descripiton:"sample Description"}
+      ],
     }
   }
   
   render(){
     console.log("screen redenering .. . . ");
     return(
-      <SafeAreaView style={{flex:1,backgroundColor:'lightgreen'}}>
-        <FlatList data={this.state.todoList} keyExtractor={(item)=>item.title} renderItem={({item})=><ToDoItem item={item}/>}/>
+      <SafeAreaView style={{flex:1}}>
+        <FlatList data={this.state.todoList} keyExtractor={(item)=>item.title} renderItem={({item})=><ToDoItem data={item}/>}/>
         <TouchableOpacity onPress={()=>{
           this.setState({
             todoList:[...this.state.todoList,{title:"Sample Title"+this.state.todoList.length,descripiton:"sample Description"}]
           });
         }}
-        style={{width:60,height:60,justifyContent:'center',alignItems:'center',background:'red',width:60,position:'absolute',bottom:60,right:60,borderRadius:30,elevation:5,elevation:5,backgroundColor:'white'}}>
+        style={{width:60,height:60,justifyContent:'center',alignItems:'center',width:60,position:'absolute',bottom:60,right:60,borderRadius:30,elevation:5,shadowOffset:{width:2,height:2},shadowRadius:5, backgroundColor:'white'}}>
           <Icon name='add-outline' size={34} color='black'/>
         </TouchableOpacity>
       </SafeAreaView>
--- src/widgets/ToDoItem.js
+++ src/widgets/ToDoItem.js
@@ -5,19 +5,19 @@ import Icon from 'react-native-vector-icons/Ionicons';
 export default class ToDoItem extends React.Component {
   constructor(props){
     super();
-    this.item = props.item;
+    this.todo = props.data;
   }    
   render(){
     return (
       <View style={{padding:10,backgroundColor:'lightpink',margin:10,borderRadius:5,shadowColor:'black',shadowOffset:{width:2,height:2},shadowRadius:2,shadowOpacity:0.5,elevation:5}}>
         <View style={{display:'flex',flexDirection:'row',justifyContent:'space-between',alignItems:'center'}}>
-        <Text>{this.item.title}</Text>
+        <Text>{this.props.data.title}</Text>
         <View style={{display:'flex',flexDirection:'row',justifyContent:'space-evenly',padding:10,width:100,backgroundColor:'green',borderRadius:25}}>
           <Icon name='checkmark-outline' type='ionicon' size={18} style={{color:'white'}}/>
           <Text style={{color:'white',fontWeight:'bold'}}>DONE</Text>
         </View>
         </View>
-        <Text>{this.item.descripiton}</Text>
+        <Text>{this.todo.descripiton}</Text>
       </View>
     );
   }
